06a89215d6194848c73f5eb3d9c8822811ca0f8e
EXA-Hub <exa4ads@gmail.com>
2025-01-14 22:58:44 +0200

    s

--- api/src/apps/discord.js
+++ api/src/apps/discord.js
@@ -1,10 +1,10 @@
 // my-api/src/apps/discord.js
 import mongoose from 'mongoose';
 const AppID = 'Discord';
-import dotenv from 'dotenv-safe';
-dotenv.config({
-  allowEmptyValues: true,
-});
+// import dotenv from 'dotenv-safe';
+// dotenv.config({
+//   allowEmptyValues: true,
+// });
 const discordApp = {
   id: AppID,
   name: 'ديسكورد',
