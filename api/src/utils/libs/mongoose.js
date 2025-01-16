e96c0e6ec6b916dd54d210ebd0a44425dfe530e0
EXA-Hub <exa4ads@gmail.com>
2025-01-17 00:20:42 +0200

    s

--- api/src/utils/libs/mongoose.js
+++ api/src/utils/libs/mongoose.js
@@ -16,10 +16,7 @@ export const connectToMongoDB = async () => {
   isConnecting = true;
 
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
   } catch (err) {
     console.error('MongoDB connection error:', err);
     throw err;
