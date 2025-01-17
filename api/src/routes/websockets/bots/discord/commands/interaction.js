120184c77d50489832267f84352c41e37efd6602
EXA-Hub <exa4ads@gmail.com>
2025-01-17 20:56:45 +0200

    all

--- api/src/routes/websockets/bots/discord/commands/interaction.js
+++ api/src/routes/websockets/bots/discord/commands/interaction.js
@@ -4,7 +4,7 @@ import { Router } from 'express';
 const router = Router();
 
 // discord Interactions Endpoint URL
-router.get('/interactions', (req, res) => {
+router.all('/interactions', (req, res) => {
   res.status(200).json({
     type: 1, // 1 for an application command
   });
