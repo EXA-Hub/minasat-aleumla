307e48b7dbd14afde806a66109a16ee0ef28638e
EXA-Hub <exa4ads@gmail.com>
2025-01-20 22:08:40 +0200

    toLocaleLowerCase

--- api/src/routes/public/connect.js
+++ api/src/routes/public/connect.js
@@ -9,7 +9,9 @@ router.get('/app/connect/:appId', (req, res) => {
   const { appId } = req.params;
 
   // Validate if the appId exists in the apps array
-  const app = apps.find((app) => app.id === appId);
+  const app = apps.find(
+    (app) => app.id.toLocaleLowerCase() === appId.toLocaleLowerCase()
+  );
 
   if (!app) return res.status(400).json({ error: 'التطبيق غير موجود', appId }); // 'App not found' in Arabic
 
