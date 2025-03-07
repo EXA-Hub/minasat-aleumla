fa7d25590f22e87e02d8a433c27b36c5cc2e2dd9
EXA-Hub <exa4ads@gmail.com>
2025-03-07 14:46:08 +0200

    Payments

--- app/src/pages/dashboard/exchange.jsx
+++ app/src/pages/dashboard/exchange.jsx
@@ -1,61 +1,20 @@
+import { Link } from 'react-router-dom';
 import { toast } from 'react-hot-toast';
 import { useEffect, useState } from 'react';
-import { Bitcoin, CopyIcon, InfoIcon } from 'lucide-react';
 import BalanceCard from '../../components/dashboard/widgets/BalanceCard';
-import { Tabs, TabsList, TabsTrigger } from '../../components/ui/tabs';
-import { Alert, AlertDescription } from '../../components/ui/alert';
-import { Textarea } from '../../components/ui/textarea';
-import CoinIcon from '../../components/ui/CoinIcon';
-import { Button } from '../../components/ui/button';
-import { Input } from '../../components/ui/input';
-import { Label } from '../../components/ui/label';
 import LoadingPage from '../core/loading';
 import api from '../../utils/api';
-import {
-  Dialog,
-  DialogTrigger,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-} from '../../components/ui/dialog';
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '../../components/ui/select';
-import {
-  Card,
-  CardHeader,
-  CardTitle,
-  CardContent,
-  CardFooter,
-} from '../../components/ui/card';
 
 const Exchange = () => {
-  const [open, setOpen] = useState(false);
-  const [exchangeData, setExchangeData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [balance, setBalance] = useState(0);
-  const [formData, setFormData] = useState({
-    action: '',
-    currency: '',
-    amount: 0,
-    walletAddress: '',
-    feePriority: 'normal',
-  });
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         setLoading(true);
-        const [balanceData, exchange] = await Promise.all([
-          api.wallet.getBalance(),
-          api.wallet.getRates(),
-        ]);
+        const [balanceData] = await Promise.all([api.wallet.getBalance()]);
         setBalance(balanceData.balance);
-        setExchangeData(exchange);
       } catch (err) {
         console.error(err);
         toast.error('حدث خطأ ما');
@@ -69,48 +28,6 @@ const Exchange = () => {
 
   if (loading) return <LoadingPage />;
 
-  const information = formData.currency
-    ? {
-        isBTC: formData.currency.toLowerCase() === 'bitcoin',
-        currencyToCoin:
-          exchangeData.data[formData.currency].usd /
-          exchangeData.exchange.coinToUsdRate,
-        min: {
-          coins:
-            exchangeData.exchange.minUsd / exchangeData.exchange.coinToUsdRate,
-          currency:
-            exchangeData.exchange.minUsd /
-            exchangeData.data[formData.currency].usd,
-        },
-        max: {
-          coins:
-            exchangeData.exchange.maxUsd / exchangeData.exchange.coinToUsdRate,
-          currency:
-            exchangeData.exchange.maxUsd /
-            exchangeData.data[formData.currency].usd,
-        },
-      }
-    : {};
-
-  const textToCopy =
-    exchangeData?.currencies[formData.currency] ||
-    'سيتم توفير عنوان المحفظة عما قريب';
-
-  const copyToClipboard = async () => {
-    try {
-      await navigator.clipboard.writeText(textToCopy);
-      toast.success('تم النسخ');
-    } catch (error) {
-      console.error('Failed to copy:', error);
-      toast.error('فشل النسخ');
-    }
-  };
-
-  const handleSubmit = () => {
-    setOpen(true);
-    setFormData({ action: false });
-  };
-
   return (
     <div className="space-y-4">
       <h2 className="text-3xl font-bold">
@@ -122,209 +39,54 @@ const Exchange = () => {
 
       <BalanceCard balance={{ amount: balance }} />
 
-      {open && (
-        <Dialog>
-          <DialogContent className="text-center">
-            <DialogHeader>
-              <DialogTitle>تنبيه</DialogTitle>
-            </DialogHeader>
-            <div className="py-4">
-              <p className="text-lg">سيتم توفير الخدمة عما قريب</p>
-            </div>
-            <DialogTrigger onClick={() => setOpen(false)}>إغلاق</DialogTrigger>
-          </DialogContent>
-        </Dialog>
-      )}
+      <div className="flex flex-col items-center gap-4">
+        {/* Payment Methods Section */}
+        <div className="text-center">
+          <h3 className="text-foreground text-2xl font-bold">
+            طرق الدفع المتاحة
+          </h3>
+          <p className="text-muted-foreground text-xl">
+            اختر طريقة الدفع المفضلة لديك
+          </p>
+        </div>
 
-      <Tabs
-        defaultValue="sell_coins"
-        value={formData.action}
-        onValueChange={(value) =>
-          setFormData((prev) => ({ ...prev, action: value }))
-        }>
-        <TabsList className="grid w-full grid-cols-2">
-          <TabsTrigger value="sell_coins">البيع</TabsTrigger>
-          <TabsTrigger value="buy_coins">الشراء</TabsTrigger>
-        </TabsList>
-      </Tabs>
-
-      {formData.action && (
-        <Tabs
-          value={formData.currency}
-          onValueChange={(value) =>
-            setFormData((prev) => ({ ...prev, currency: value }))
-          }>
-          <TabsList className="grid w-full grid-cols-2">
-            {Object.keys(exchangeData.data).map((currency) => (
-              <TabsTrigger value={currency} key={currency}>
-                {currency}
-              </TabsTrigger>
-            ))}
-          </TabsList>
-        </Tabs>
-      )}
-
-      {formData.currency && (
-        <Card>
-          <CardHeader className="bg-background m-3 overflow-x-auto rounded-md px-3 py-2 whitespace-nowrap sm:px-6 sm:py-4">
-            <CardTitle className="text-base sm:text-lg">
-              معلومات العملية
-            </CardTitle>
-            <div className="flex flex-col gap-y-2 text-xs sm:text-sm">
-              {/* Price Row */}
-              <div className="flex items-center gap-1 sm:gap-2">
-                <span className="whitespace-nowrap">
-                  السعر: 1 {formData.currency} =
-                </span>
-                <CoinIcon
-                  className="h-4 w-4 sm:h-5 sm:w-5"
-                  amount={Math.round(information.currencyToCoin)}
-                />
-              </div>
-
-              {/* Min/Max Rows */}
-              {['min', 'max'].map((type) => (
-                <div key={type} className="flex items-center gap-1 sm:gap-2">
-                  <span className="whitespace-nowrap">
-                    {type === 'min' ? 'الحد الأدنى' : 'الحد الأقصى'}:{' '}
-                    {exchangeData.exchange[`${type}Usd`]}$ =
-                  </span>
-                  <div className="flex items-center gap-1">
-                    <CoinIcon
-                      className="h-4 w-4 sm:h-5 sm:w-5"
-                      amount={Math[type === 'min' ? 'ceil' : 'floor'](
-                        information[type].coins
-                      )}
-                    />
-                    <span>=</span>
-                    {information.isBTC ? (
-                      <Bitcoin className="h-3 w-3 sm:h-4 sm:w-4" />
-                    ) : (
-                      <span>$</span>
-                    )}
-                    <span className="break-all">
-                      {information[type].currency.toFixed(
-                        information.isBTC ? 8 : 2
-                      )}
-                    </span>
-                  </div>
-                </div>
-              ))}
-
-              {/* Amount Row */}
-              <div className="flex items-center gap-1 sm:gap-2">
-                <span>المبلغ:</span>
-                <div className="flex items-center gap-1">
-                  <CoinIcon
-                    className="h-4 w-4 sm:h-5 sm:w-5"
-                    amount={Number.parseInt(formData.amount)}
-                  />
-                  <span>=</span>
-                  {information.isBTC ? (
-                    <Bitcoin className="h-3 w-3 sm:h-4 sm:w-4" />
-                  ) : (
-                    <span>$</span>
-                  )}
-                  <span className="break-all">
-                    {(formData.amount / information.currencyToCoin).toFixed(
-                      information.isBTC ? 8 : 2
-                    )}
-                  </span>
-                </div>
-              </div>
-            </div>
-          </CardHeader>
-
-          <CardContent className="space-y-4">
-            <div className="flex flex-col gap-y-2">
-              <Label htmlFor="amount">المبلغ</Label>
-              <Input
-                id="amount"
-                type="number"
-                value={formData.amount}
-                min={Math.ceil(information.min.coins)}
-                max={Math.floor(information.max.coins)}
-                onChange={(e) =>
-                  setFormData((prev) => ({ ...prev, amount: e.target.value }))
-                }
+        {/* Payment Icons Grid */}
+        <div className="grid grid-cols-2 gap-6 sm:grid-cols-3 lg:grid-cols-4">
+          {[
+            {
+              to: '/pay/paypal',
+              src: '/icons/payment/paypal.png',
+              alt: 'PayPal Payment',
+            },
+            {
+              to: '/pay/vodafone-cash',
+              src: '/icons/payment/vodafone.png',
+              alt: 'Vodafone Cash',
+            },
+            {
+              to: '/pay/crypto',
+              src: '/icons/payment/crypto.png',
+              alt: 'Cryptocurrency',
+            },
+            {
+              to: '/pay/probot',
+              src: '/icons/payment/Probot.png',
+              alt: 'Probot Payment',
+            },
+          ].map((payment, index) => (
+            <Link
+              key={index}
+              to={payment.to}
+              className="group bg-30mutedforeground flex items-center justify-center rounded-lg">
+              <img
+                src={payment.src}
+                className="object-contain p-2"
+                alt={payment.alt}
               />
-            </div>
-
-            <div className="flex flex-col gap-y-2">
-              <Label htmlFor="wallet" className="flex items-center gap-2">
-                معلومات التحويل
-                <span className="text-muted-foreground text-xs">
-                  (الرجاء التأكد من المعلومات قبل إدخالها)
-                </span>
-              </Label>
-              <Textarea
-                id="wallet"
-                value={formData.walletAddress}
-                onChange={(e) =>
-                  setFormData((prev) => ({
-                    ...prev,
-                    walletAddress: e.target.value,
-                  }))
-                }
-              />
-            </div>
-
-            {formData.action === 'sell_coins' ? (
-              <div className="flex flex-col gap-y-2">
-                <Label htmlFor="fee">الرسوم</Label>
-                <Select
-                  value={formData.feePriority}
-                  onValueChange={(value) =>
-                    setFormData((prev) => ({ ...prev, feePriority: value }))
-                  }>
-                  <SelectTrigger id="fee">
-                    <SelectValue placeholder="اختر نوع الرسوم" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem value="normal">عادي</SelectItem>
-                    <SelectItem value="fast">سريع (رسوم تحويل أعلى)</SelectItem>
-                    <SelectItem value="economic">
-                      إقتصادي (رسوم منخفضة وتأخير عند الوصول)
-                    </SelectItem>
-                  </SelectContent>
-                </Select>
-              </div>
-            ) : (
-              <div className="flex flex-col gap-y-2">
-                <Label>قم بالتحويل على هذه المحفظة</Label>
-                <div className="flex items-center gap-2 space-x-2">
-                  <Input
-                    readOnly
-                    value={textToCopy}
-                    className="rounded-none font-mono"
-                  />
-                  <Button
-                    size="icon"
-                    variant="outline"
-                    onClick={copyToClipboard}
-                    className="border-border shrink-0 rounded-none border">
-                    <CopyIcon className="h-4 w-4" />
-                  </Button>
-                </div>
-              </div>
-            )}
-          </CardContent>
-
-          <CardFooter className="flex-col gap-5">
-            <Alert
-              variant="warning"
-              className="flex items-center rounded-lg bg-yellow-100 p-4 text-yellow-800 shadow-md">
-              <InfoIcon className="mr-3 h-6 w-6" />
-              <AlertDescription className="text-sm">
-                كامل العمليات تكون شاملة رسوم التحويل من طرف المستخدم
-              </AlertDescription>
-            </Alert>
-            <Button className="w-full" variant="outline" onClick={handleSubmit}>
-              إرسال الطلب
-            </Button>
-          </CardFooter>
-        </Card>
-      )}
+            </Link>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
