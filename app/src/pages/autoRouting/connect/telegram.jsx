49dd76142f8e5733add5cf507cb033f8a68cf26b
EXA-Hub <exa4ads@gmail.com>
2025-03-09 02:33:38 +0200

    MFA

--- app/src/pages/autoRouting/connect/telegram.jsx
+++ app/src/pages/autoRouting/connect/telegram.jsx
@@ -9,16 +9,14 @@ export default function TelegramAuth() {
   const [mfa, setMfa] = useState(false);
   const [responseStatus, setResponseStatus] = useState(null);
   useEffect(() => {
-    if (!mfa) {
-      const script = document.createElement('script');
-      script.src = 'https://telegram.org/js/telegram-widget.js?22';
-      script.async = true;
-      script.setAttribute('data-telegram-login', 'Minasat_Aleumla_bot');
-      script.setAttribute('data-size', 'large');
-      script.setAttribute('data-request-access', 'write');
-      script.setAttribute('data-onauth', 'onTelegramAuth(user)');
-      document.getElementById('telegram-login').appendChild(script);
-    }
+    const script = document.createElement('script');
+    script.src = 'https://telegram.org/js/telegram-widget.js?22';
+    script.async = true;
+    script.setAttribute('data-telegram-login', 'Minasat_Aleumla_bot');
+    script.setAttribute('data-size', 'large');
+    script.setAttribute('data-request-access', 'write');
+    script.setAttribute('data-onauth', 'onTelegramAuth(user)');
+    document.getElementById('telegram-login').appendChild(script);
     window.onTelegramAuth = (user) => {
       setDataSent(true);
       setLoading(true);
